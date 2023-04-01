ca39a9c22a6d70d36158df648d28f937fb7d0282
Saumya <saumyaverma236@gmail.com>
2023-03-31 22:15:55 -0400

    moving login code to users

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-// import userRoutes from './users.js';
+ import userRoutes from './users.js';
 // import songRoutes from './songs.js';
 // import artistRoutes from './artistRoutes.js';
 // import commentRoutes from './comments.js';
@@ -6,8 +6,9 @@
 const constructorMethod = (app) => {
     app.get('/', (req, res) => {
         // Landing page
+        res.send("Hello User");
     });
-    // app.use('/users', userRoutes);
+    app.use('/users', userRoutes);
     // app.use('/songs', songRoutes);
     // app.use('/artists', artistRoutes);
     // app.use('/comments', commentRoutes);
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,133 @@
 import { Router } from 'express';
 import { ObjectId } from 'mongodb';
 import { userData } from '../data/index.js';
+import querystring from 'querystring';
+import request from 'request'; // "Request" library
+
+
+const router = Router();
+
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+  
+  var stateKey = 'spotify_auth_state';
+
+router
+      .route('/login')
+      .get(async (req, res) => {
+        
+     var state = generateRandomString(16);
+     res.cookie(stateKey, state);
+
+  // your application requests authorization
+     var scope = 'user-read-private user-read-email';
+     res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+  });
+
+router.get('/callback', function(req, res) {
+     // your application requests refresh and access tokens
+  // after checking the state parameter
+  
+  
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  console.log(state)
+  // var storedState = req.cookies ? req.cookies[stateKey] : null;
+  // console.log(storedState)
+
+  if (state === null ) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+         // url : 'https://api.spotify.com/v1/tracks/11dFghVXANMlKmJXsNCbNl',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+router.get('/refresh_token', function(req, res) {
+
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+      form: {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token
+      },
+      json: true
+    };
+  
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token;
+        res.send({
+          'access_token': access_token
+        });
+      }
+    });
+  });
+
+  export default router;
