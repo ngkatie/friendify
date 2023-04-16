929dbfd5a571a18d2dd0c2433dd69c0ee0241cf7
Katie Ng <ngkatie16@gmail.com>
2023-04-16 15:24:47 -0400

    added getTopTracks and getTopArtists

--- data/users.js
+++ data/users.js
@@ -1,6 +1,6 @@
 import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
-//import { hashPassword } from "../config/password.js";
+import * as helpers from "../helpers.js"
 
 const create = async (
     username,
@@ -9,45 +9,45 @@ const create = async (
     hashed_password
 ) => {
     //function that hashes the password
-    hashed_password = hashPassword(hashed_password);
-let newUser = {
-    username: username,
-    email: email,
-    spotify_access_token: spotify_access_token,
-    hashed_password: hashed_password,
-    top_songs: [],
-    top_artists: [],
-    dailyPlaylist: [],
-    likeCount: 0,
-    comments: [],
-    likedProfiles: [],
-    pendingRequests: [],
-    friends: [],
-}
-const userCollection = await users();
-const insertInfo = await userCollection.insertOne(newUser);
-const newId = insertInfo.insertedId.toString();
-const user = await get(newId);
-return user;
+    hashed_password = helpers.hashPassword(hashed_password);
+    let newUser = {
+        username: username,
+        email: email,
+        spotify_access_token: spotify_access_token,
+        hashed_password: hashed_password,
+        top_songs: [],
+        top_artists: [],
+        dailyPlaylist: [],
+        likeCount: 0,
+        comments: [],
+        likedProfiles: [],
+        pendingRequests: [],
+        friends: [],
+    }
+    const userCollection = await users();
+    const insertInfo = await userCollection.insertOne(newUser);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+        throw `Could not add user successfully`;
+      }
+
+    // const newId = insertInfo.insertedId.toString();
+    const user = await get(insertInfo.insertedId.toString());
+    return helpers.idToString(user);
 }
 
 const getAll = async () => {
-    const userCollection = await bands();
+    const userCollection = await users();
     let userList = await userCollection.find({}).toArray();
-    if(!userList) throw 'Could not get all users';
-    userList = userList.map((user) => {
-        user._id = user._id.toString();
-        return user;
-    })
+    if(!userList) {throw 'Could not get all users'};
+    userList = userList.map(helpers.idToString);
     return userList;
 }
 
 const get = async (id) => {
-    id = id.trim();
+    id = helpers.checkId(id);
     const userCollection = await users();
-    const user = await userCollection.findOne({ _id: ObjectId(id) });
-    user._id = user._id.toString();
-    return user;   
+    const user = await userCollection.findOne({ _id: id });
+    return helpers.idToString(user);
 }
 
 export {create, getAll, get}
--- helpers.js
+++ helpers.js
@@ -3,14 +3,29 @@ import axios from 'axios'; // "Request" library
 //This file contains any functions that are too long or can be used in multiple places
 
 //function to hash a password
-function hashPassword(password) {
+export function hashPassword(password) {
   const sha256 = crypto.createHash('sha256');
   sha256.update(password);
   const hashedPassword = sha256.digest('hex');
   return hashedPassword;
 }
 
+export function checkString(str) {
+  if (!str || typeof str !== `string` || str.trim().length === 0) {
+    throw `Error: ${str} must be a non-empty string`;
+  }
+  return str.trim();
+};
 
+export function idToString(mongoDocument) {
+  mongoDocument._id = mongoDocument._id.toString();
+  return mongoDocument;
+}
 
-
-export { hashPassword };
+export function checkId(str) {
+  const idString = checkString(str);
+  const id = new ObjectId(str);
+  if (!ObjectId.isValid(id)) {throw `Error: Invalid object ID`};
+  // Returns ID as ObjectId, not string
+  return id;
+}
